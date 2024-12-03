f62d0a92742a5daf631e029bfeda1b8778c351ef
Merbo <ihavenomoneyforgacha@gmail.com>
2024-12-03 16:58:40 +0100

    omjiko

---
+++ src/hilos/src/DentroFuera.java
@@ -0,0 +1,10 @@
+
+public class DentroFuera {
+    public static void main(String[] args) {
+        HiloFor hf = new HiloFor();
+        hf.start();
+        for (int i = 0; i < 10; i++){
+            System.out.println("Estoy fuera del hilo");
+        }
+    }
+}
